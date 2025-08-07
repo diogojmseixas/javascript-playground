17a0c1433ad7e0dda45659ca4e00930652ab9a78
diogojmseixas <diogojmseixas7@gmail.com>
2025-08-07 09:56:07 +0100

    sandbox-004

---
+++ sandbox-004/sandbox.js
@@ -0,0 +1,17 @@
+// common strings methods
+
+let email = "mail@mail.com";
+
+//let result = email.lastIndexOf("l");
+
+//let result = email.slice(0, 4);
+
+//let result = email.substring(5, 9);
+
+//let result = email.replace("@", "$");
+
+//let result = email.replace("m", "n");
+
+let result = email.replaceAll("m", "n");
+
+console.log(result);
