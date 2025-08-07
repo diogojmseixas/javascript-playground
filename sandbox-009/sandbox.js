fa3b15fdb8e81e680f62e40f0b790f8ee742b33d
diogojmseixas <diogojmseixas7@gmail.com>
2025-08-07 14:02:12 +0100

    sandbox-009

---
+++ sandbox-009/sandbox.js
@@ -0,0 +1,30 @@
+//Booleans & comparisons
+
+console.log(true, false, "true", "false");
+
+//Methods can return booleans
+
+let email = "mail@mail.com";
+let names = ["Paul", "John", "Jackson"];
+
+//let result = email.includes("$");
+//let result = names.includes("John");
+
+//console.log(result);
+
+//Comparison operators
+
+let age = 40;
+
+/* console.log(age == 20);
+console.log(age == 30);
+console.log(age != 30);
+console.log(age > 30);
+console.log(age < 30);
+console.log(age <= 30);
+console.log(age >= 30); */
+
+let name = "Edward";
+
+console.log(name == "edward");
+console.log(name == "Edward");
