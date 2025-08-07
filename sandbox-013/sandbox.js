04d8e7aafacb43be58f6037ef3d612b2d091bc45
diogojmseixas <diogojmseixas7@gmail.com>
2025-08-07 14:26:54 +0100

    sandbox-013

---
+++ sandbox-013/sandbox.js
@@ -0,0 +1,18 @@
+//While loops
+
+const names = ["John", "Edward", "Jackson"];
+
+/* let i = 0;
+
+while (i < 5) {
+    console.log("In loop: ", i);
+    i++;
+}
+ */
+
+let i = 0;
+
+while (i < names.length) {
+    console.log(names[i]);
+    i++;
+}
