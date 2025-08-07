b1cee939c761fafd562a6ee958bd29c9ae4c00c6
diogojmseixas <diogojmseixas7@gmail.com>
2025-08-07 13:38:44 +0100

    sandbox-007

---
+++ sandbox-007/sandbox.js
@@ -0,0 +1,22 @@
+let names = ["John", "Jackson", "Carl"];
+
+//names[1] = 'Paul';
+//console.log(names[1]);
+
+//let ages = [20, 40, 30];
+//console.log(ages[2]);
+
+//let random = ["Harry", "William", 50, 60];
+//console.log(random);
+
+//console.log(names.length);
+
+//Array Methods
+
+//let result = names.join("-");
+//let result= names.indexOf('Jackson');
+//let result = names.concat(['Arthur', 'Edward']);
+let result = names.push("Lily");
+result = names.pop();
+
+console.log(result);
