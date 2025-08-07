36d36bf43183b589d027a20c2a26d3a98b78beca
diogojmseixas <diogojmseixas7@gmail.com>
2025-08-07 14:13:06 +0100

    sandbox-011

---
+++ sandbox-011/sandbox.js
@@ -0,0 +1,19 @@
+//Type convertion
+
+let score = "100";
+
+//score = Number(score);
+
+//console.log(score + 1);
+
+//console.log(typeof score);
+
+//let result = Number("Hello");
+
+//let result = String("50");
+
+//let result = Boolean(0);
+
+let result = Boolean("");
+
+console.log(result, typeof result);
