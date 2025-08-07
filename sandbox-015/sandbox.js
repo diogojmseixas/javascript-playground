c4b8397643aef5d58d8e5d8599ca09d211fc6a26
diogojmseixas <diogojmseixas7@gmail.com>
2025-08-07 15:09:23 +0100

    sandbox-015

---
+++ sandbox-015/sandbox.js
@@ -0,0 +1,20 @@
+//If statements
+
+/* const age = 40;
+
+if (age > 20) {
+    console.log("You are over 20 years old.");
+}
+
+const names = ["Paul", "William", "Jackson", "Edward"];
+
+if (names.length > 3) {
+    console.log("That's a lot of people.");
+}
+ */
+
+const password = "password";
+
+if (password.length >= 8) {
+    console.log("That password is long enough!");
+}
