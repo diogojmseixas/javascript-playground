f0883672545f45d4ef60316f47896e901c4ac43b
diogojmseixas <diogojmseixas7@gmail.com>
2025-08-07 09:48:36 +0100

    sandbox-003

---
+++ sandbox-003/sandbox.js
@@ -0,0 +1,34 @@
+// Strings
+
+console.log("Hello world!");
+
+let email = "mail@mail.com";
+console.log(email);
+
+// String Concatenation
+
+let firstName = "John";
+let lastName = "Smith";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// Getting Characters
+
+console.log(fullName[0]);
+
+// String Length
+
+console.log(fullName.length);
+
+// String Methods
+
+console.log(fullName.toUpperCase());
+
+let result = fullName.toLowerCase();
+
+console.log(result, fullName);
+
+let index = email.indexOf("@");
+
+console.log(index);
