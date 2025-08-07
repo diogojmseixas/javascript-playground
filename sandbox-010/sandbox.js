d6cff6c27a678b9c804382b5335ebed9a74717e6
diogojmseixas <diogojmseixas7@gmail.com>
2025-08-07 14:06:38 +0100

    sandbox-010

---
+++ sandbox-010/sandbox.js
@@ -0,0 +1,16 @@
+let age = 25;
+
+//Loose comparison (different types can still be equal)
+
+//console.log(age == 25);
+//console.log(age == "25");
+
+//console.log(age != "25");
+//console.log(age != "25");
+
+//Strict comparasion (different types cannot be equal)
+
+console.log(age === 25);
+console.log(age === "25");
+console.log(age !== 25);
+console.log(age !== "25");
